0addb60b02286e3787c8340692bb338c73c47889
Hunter Hampton <hmh84@drexel.edu>
2021-09-15 23:13:48 -0400

    Hosting website

--- server.js
+++ server.js
@@ -140,3 +140,20 @@ function stop(message, serverQueue) {
 }
 
 client.login(token);
+
+// Start website
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('I\'m not really a website. Go away.');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
